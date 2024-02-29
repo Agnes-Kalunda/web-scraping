a3a207635237e8d4b7286012fc43d84476bf78f6
Agnes <agypeter97@gmail.com>
2024-02-29 23:22:51 +0300

    user-agent headers to mimic web browser

--- webScrapingApp/views.py
+++ webScrapingApp/views.py
@@ -8,11 +8,18 @@ from plyer import notification
 def get_machine_learning_news(request):
     location = "en-KE"  
     url = f"https://news.google.com/search?q=machine+learning&hl={location}"
-    response = requests.get(url)
+
+
+#user-agent headers
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
-
+     
         news_titles = [item.text for item in soup.find_all('h3')]
         return render(request, 'main.html', {'news_titles': news_titles})
     else:
